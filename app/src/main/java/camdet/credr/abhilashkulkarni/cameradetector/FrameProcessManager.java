1218c0c8165c6c8b010df3124db966ade83803c3
abhilashsk <abhilashkulkarni1410@gmail.com>
2015-06-24 12:47:19 +0530

    Bug in cvtColor2

--- app/src/main/java/camdet/credr/abhilashkulkarni/cameradetector/FrameProcessManager.java
+++ app/src/main/java/camdet/credr/abhilashkulkarni/cameradetector/FrameProcessManager.java
@@ -26,7 +26,6 @@ public class FrameProcessManager{
     static String TAG = "Check";
     static private List<QueueElement> PendingQueue;
     static private List<QueueElement> ProcessingQueue;
-
     public FrameProcessManager(Activity context){
         this.activity = context;
         PendingQueue = new LinkedList<>();;
@@ -49,42 +48,64 @@ public class FrameProcessManager{
         CameraTraceView.CvCameraViewFrame inputFrame;
         Mat outputFrame;
         Processed callback;
+        boolean canCalculate;
+        boolean calculated;
 
         QueueElement(CameraTraceView.CvCameraViewFrame inputFrame, Processed callback) {
             sequenceId = SEQUENCE++;
             this.inputFrame = inputFrame;
             this.callback = callback;
+            Log.d("WTF","ID: " +  sequenceId);
+            calculated = false;
+
+        }
+
+        public void setCanCalculate(boolean val){
+            this.canCalculate = val;
         }
     }
        public Mat getLastFrame(){
             if(PendingQueue.size()>0)
-                return PendingQueue.get(PendingQueue.size()-1).inputFrame.rgba();
+                return PendingQueue.get(0).inputFrame.rgba();
            if(ProcessingQueue.size()>0)
-               return ProcessingQueue.get(ProcessingQueue.size()-1).inputFrame.rgba();
+               return ProcessingQueue.get(0).inputFrame.rgba();
            return null;
         }
+
     public void addFrameToQueue(CameraTraceView.CvCameraViewFrame inputFrame, Processed listener) {
+        if(PendingQueue.size() > 50)
+        {PendingQueue.remove(PendingQueue.size()-1);}
         PendingQueue.add(new QueueElement(inputFrame, listener));
+
+
         Log.d("WTF","PendingQueue: " + PendingQueue.size());
         triggerProcessing();
-    }
+}
 
     synchronized private void triggerProcessing() {
         if(PendingQueue.size() == 0 || !ProcessingManager.hasIdleThreads())
             return;
         QueueElement element = PendingQueue.get(0);
         if(ProcessingManager.processFrame(element)) {
-            PendingQueue.remove(element);
+            PendingQueue.remove(0);
             ProcessingQueue.add(element);
             Log.d("WTF","ProcessingQueue: " + ProcessingQueue.size());
         }
     }
 
     static class ProcessingManager {
+        static long lastDeliveredSequenceId = 0;
         static List<Worker> workerList = new ArrayList();
+        static List<Thread> threadList = new ArrayList();
         static {
             for(int i = 0; i < 4; i++) {
                 workerList.add(new Worker());
+            }
+        }
+
+        static {
+            for(int i = 0; i < 4; i++) {
+                threadList.add(new Thread());
             }
         }
 
@@ -97,36 +118,57 @@ public class FrameProcessManager{
         }
 
         synchronized static boolean processFrame (QueueElement element) {
+            long id;
             for(Worker worker : workerList) {
                 if(!worker.busy) {
+                    id = element.sequenceId;
+                    if(id % 10 == 1)
+                        element.canCalculate = true;
+                    else
+                        element.canCalculate = false;
                     worker.setFrameElement(element);
                     Thread WorkerThread = new Thread(worker);
                     WorkerThread.start();
                     return true;
                 }
             }
+
+            /*for(Thread thread : threadList) {
+                if (thread.getState() != Thread.State.RUNNABLE){
+                    Worker worker = new Worker();
+                worker.setFrameElement(element);
+                thread.run();
+            }
+
+
+            }*/
             return false;
         }
 
         synchronized static void processingCompleted(final QueueElement element) {
-            activity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    element.callback.onProcessComplete(element.outputFrame);
-                    ProcessingQueue.remove(element);
-                }
-            });
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("WTF","Element_ID: " + element.sequenceId + " Last_ID: " + lastDeliveredSequenceId);
+                        if(element.sequenceId > lastDeliveredSequenceId ){
+                        element.callback.onProcessComplete(element.outputFrame);
+                            lastDeliveredSequenceId = element.sequenceId;}
+                        if(element.sequenceId >= lastDeliveredSequenceId )
+                        ProcessingQueue.remove(element);
+                    }
+                });
         }
 
         public static class Worker implements Runnable {
-            private boolean canShow = true, canCopy = true, copied = true;
+            private boolean canShow = true, cannied = true;
             private int k = 0;
-            private int i = 10, width = 640, height = 480,j,largest_area=0,largest_contour_index = 0,area = 0;
+            private int largest_area=0,largest_contour_index = 0,area = 0;
             private int KERNEL_SIZE = 9, BOX_SIZE = 4, vals;
             private ArrayList<double[][]> window;
             private int x_val,y_val;
             private Mat real,merged,canny,dst,thr;
-            private List<MatOfPoint> contours;
+            private List<MatOfPoint> contours = new ArrayList<>();
+            private long id;
 
             boolean busy = false;
             QueueElement element;
@@ -140,27 +182,36 @@ public class FrameProcessManager{
 
             @Override
             public void run() {
+                id = element.sequenceId;
+                busy = true;
                 real = element.inputFrame.rgba();
-                if (i == 10) {
-                    if(copied) {
-                        canCopy = true;
-                        copied = false;
-                    }
+                if(element.canCalculate) {
                     merged = real.clone();
-                    contours = new ArrayList<>();
-                    canny = element.inputFrame.gray();
-                    dst = new Mat(canny.rows(),canny.cols(), CvType.CV_8UC1);
-                    Imgproc.Canny(canny, canny, 60, 100, 3, true);
 
-                        thr = canny.clone();
+                    canny = element.inputFrame.gray();
+                    dst = new Mat(canny.rows(), canny.cols(), CvType.CV_8UC1);
+                    if (cannied) {
+                        Imgproc.Canny(canny, canny, 60, 100, 3, true);
+                        cannied = false;
+                        Log.d("WTF1", "Cannied was true");
+                    }
+                    if (!cannied) {
+                        Log.d("WTF1", "Cannied was false");
+
                         dst = canny.clone();
 
-                    Imgproc.findContours(dst, contours, new Mat(), Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
-                    cvtColor(canny, canny, COLOR_GRAY2RGBA, 4);
-                    Log.d(TAG, "Countours Size: " + contours.size());
-                    largest_area=0;
+                        Imgproc.findContours(dst, contours, new Mat(), Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+                        try {
+                            cvtColor(canny, canny, COLOR_GRAY2RGBA, 4);
+                        } catch (Exception e) {
+                            Log.e("WTF", e.getMessage());
+                        }
+                        Log.d(TAG, "Countours Size: " + contours.size());
+                        element.calculated = true;
+                    }
+                    largest_area = 0;
                     largest_contour_index = 0;
-                    for(int j=0; j< contours.size();j++) {
+                    for (int j = 0; j < contours.size(); j++) {
                         area = (int) Imgproc.contourArea(contours.get(j));
                         if (area < 200)
                             continue;
@@ -170,92 +221,97 @@ public class FrameProcessManager{
                         } else
                             continue;
                     }
-                    window = new ArrayList<>();
+                    //window = new ArrayList<>();
                     //Log.e("Values","Point: " + contours.get(largest_contour_index).get(0,0)[0] + " : " + contours.get(largest_contour_index).get(0,0)[1]);
                     //Log.e("Values", "Index:" + largest_contour_index);
-                    if(contours.size()>0){
-                    double[][] data = new double[BOX_SIZE][BOX_SIZE];
-                    for(int x = 0; x < KERNEL_SIZE;x++) {
-                        for (int y = 0; y < BOX_SIZE; y++)
-                            for (int z = 0; z < BOX_SIZE; z++) {
-                                try {
-                                    switch(x){
-                                        case 0:
-                                            x_val = y + BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[0];
-                                            y_val = z + (int)contours.get(largest_contour_index).get(0,0)[1];
-                                            break;
-                                        case 1:
-                                            x_val = y + BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[0];
-                                            y_val = z + BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[1];
-                                            break;
-                                        case 2:
-                                            x_val = y  + (int)contours.get(largest_contour_index).get(0,0)[0];
-                                            y_val = z + BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[1];
-                                            break;
-                                        case 3:
-                                            x_val = y - BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[0];
-                                            y_val = z + BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[1];
-                                            break;
-                                        case 4:
-                                            x_val = y - BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[0];
-                                            y_val = z + (int)contours.get(largest_contour_index).get(0,0)[1];
-                                            break;
-                                        case 5:
-                                            x_val = y - BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[0];
-                                            y_val = z - BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[1];
-                                            break;
-                                        case 6:
-                                            x_val = y  + (int)contours.get(largest_contour_index).get(0,0)[0];
-                                            y_val = z - BOX_SIZE +  (int)contours.get(largest_contour_index).get(0,0)[1];
-                                            break;
-                                        case 7:
-                                            x_val = y + BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[0];
-                                            y_val = z - BOX_SIZE + (int)contours.get(largest_contour_index).get(0,0)[1];
-                                            break;
+                    /*if (contours.size() > 0) {
+                        double[][] data = new double[BOX_SIZE][BOX_SIZE];
+                        for (int x = 0; x < KERNEL_SIZE; x++) {
+                            for (int y = 0; y < BOX_SIZE; y++)
+                                for (int z = 0; z < BOX_SIZE; z++) {
+                                    try {
+                                        switch (x) {
+                                            case 0:
+                                                x_val = y + BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[0];
+                                                y_val = z + (int) contours.get(largest_contour_index).get(0, 0)[1];
+                                                break;
+                                            case 1:
+                                                x_val = y + BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[0];
+                                                y_val = z + BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[1];
+                                                break;
+                                            case 2:
+                                                x_val = y + (int) contours.get(largest_contour_index).get(0, 0)[0];
+                                                y_val = z + BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[1];
+                                                break;
+                                            case 3:
+                                                x_val = y - BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[0];
+                                                y_val = z + BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[1];
+                                                break;
+                                            case 4:
+                                                x_val = y - BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[0];
+                                                y_val = z + (int) contours.get(largest_contour_index).get(0, 0)[1];
+                                                break;
+                                            case 5:
+                                                x_val = y - BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[0];
+                                                y_val = z - BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[1];
+                                                break;
+                                            case 6:
+                                                x_val = y + (int) contours.get(largest_contour_index).get(0, 0)[0];
+                                                y_val = z - BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[1];
+                                                break;
+                                            case 7:
+                                                x_val = y + BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[0];
+                                                y_val = z - BOX_SIZE + (int) contours.get(largest_contour_index).get(0, 0)[1];
+                                                break;
+                                        }
+                                        data[y][z] = thr.get(x_val, y_val)[0];
+                                    } catch (NullPointerException e) {
                                     }
-                                    data[y][z] = thr.get(x_val,y_val)[0];
-                                } catch (NullPointerException e) {
-                                    Log.e(TAG, "Error");
                                 }
-                            }
-                        Log.d("Values","Data " + x + " added");
-                        window.add(x, data);
-                    }
-                    vals = 0;
-                    for(int k = 0;k < window.size();k++)
-                        for(int l = 0;l < 4;l++)
-                            for(int m = 0;m < 4;m++) {
-                                if(window.get(k)[l][m]==255.0)
-                                    //Log.d("Values", "Vector: " + k + " Row: " + l + " Column: " + m + " Value: " + window.get(k)[l][m]);
-                                    vals++;
-                            }
-                    Log.d("Values", "No of values: " + vals);
-                    copied = true;}
+                            Log.d("Values", "Data " + x + " added");
+                            window.add(x, data);
+                        }
+                        vals = 0;
+                        for (int k = 0; k < window.size(); k++)
+                            for (int l = 0; l < 4; l++)
+                                for (int m = 0; m < 4; m++) {
+                                    if (window.get(k)[l][m] == 255.0)
+                                        //Log.d("Values", "Vector: " + k + " Row: " + l + " Column: " + m + " Value: " + window.get(k)[l][m]);
+                                        vals++;
+                                }
+                        Log.d("Values", "No of values: " + vals);
 
-                    if(contours.size() != 0) {
+                    }
+
+                    if (contours.size() != 0) {
                         Rect rect = Imgproc.boundingRect(contours.get(largest_contour_index));
 
                         //System.out.println(rect.height);
                         //Log.d(TAG,rect.x + "," + rect.y + "," + rect.height + "," + rect.width);
                         Imgproc.rectangle(canny, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255));
+                    }*/
+
+
+                    try {
+                        addWeighted(real, 1.0, canny, 1.0, 0.0, merged);
+                    } catch (CvException e) {
+                        Log.e(TAG, e.getMessage());
                     }
                 }
-                try {
-                    addWeighted(real, 1.0, canny, 1.0, 0.0, merged);
-                }
-                catch(CvException e){
-                    Log.e(TAG,e.getMessage());
-                }
-                i--;
-                if (i == 0)
-                    i = 10;
-                k = 1;
-                if(canShow) {
-                    element.outputFrame = merged;
+                else
+                    element.calculated = true;
+
+
+                    k = 1;
+                Log.d("WTF1","Element: " + element.calculated);
+                if(element.calculated){
+                    cannied = true;
+                    element.outputFrame = canny;
                 }
                 else
                     element.outputFrame = real;
                 processingCompleted(element);
+                busy = false;
             }
         }
     }
